dc31aff4b6135f75015969e04bf4f792dcd5a011
Sahil Jajodia <sahil.jajodia@gmail.com>
2018-12-19 23:47:51 +0530

    Changed analytics view

--- appOne/views.py
+++ appOne/views.py
@@ -65,129 +65,161 @@ def index(request):
 
 
 
-def analytics(request):
+def analytics(request, subjects):
+    #To be integrated with the frontend
+
     if Feedback.objects.all().count() != 0:
+        final = []
+        subject_list = []
         if request.user.is_authenticated():
-            final = []
-            subject = []
-            # CALCULATIONS FOR AM3
-            if Feedback.objects.all().filter(subject='Applied Mathematics - III', user=request.user).count() != 0:
-                am3 = Feedback.objects.all().filter(subject='Applied Mathematics - III', user=request.user)
-                avg_am3 = []
-                for i in range(0, len(am3)):
-                    sum = 0
-                    sum = am3[i].res1 + am3[i].res2 + am3[i].res3 + am3[i].res4 + am3[i].res5 + am3[i].res6 + am3[i].res7 + am3[i].res8 + am3[i].res9
-                    avg = sum / 9
-                    avg_am3.append(avg)
-                sum_am3 = 0
-                for i in range(0, len(avg_am3)):
-                    sum_am3 += avg_am3[i]
-                av_am3 = sum_am3 / len(avg_am3)
-                print(av_am3)
-                final.append(int((av_am3 / 5) * 100))
-                subject.append('AM3')
-
-            # CALCULATIONS FOR AOA
-            if Feedback.objects.all().filter(subject='Analysis of Algorithms', user=request.user).count() != 0:
-                aoa = Feedback.objects.all().filter(subject='Analysis of Algorithms', user=request.user)
-                avg_aoa = []
-                for i in range(0, len(aoa)):
-                    sum = 0
-                    sum = aoa[i].res1 + aoa[i].res2 + aoa[i].res3 + aoa[i].res4 + aoa[i].res5 + aoa[i].res6 + aoa[i].res7 + aoa[i].res8 + aoa[i].res9
-                    avg = sum / 9
-                    avg_aoa.append(avg)
-                sum_aoa = 0
-                for i in range(0, len(avg_aoa)):
-                    sum_aoa += avg_aoa[i]
-                av_aoa = sum_aoa / len(avg_aoa)
-                print(av_aoa)
-                final.append(int((av_aoa / 5) * 100))
-                subject.append('AOA')
-
-            # CALCULATIONS FOR coa
-            if Feedback.objects.all().filter(subject='Computer Organization and Architecture', user=request.user).count() != 0:
-                coa = Feedback.objects.all().filter(subject='Computer Organization and Architecture', user=request.user)
-                avg_coa = []
-                for i in range(0, len(coa)):
-                    sum = 0
-                    sum = coa[i].res1 + coa[i].res2 + coa[i].res3 + coa[i].res4 + coa[i].res5 + coa[i].res6 + coa[i].res7 + coa[i].res8 + coa[i].res9
-                    avg = sum / 9
-                    avg_coa.append(avg)
-                sum_coa = 0
-                for i in range(0, len(avg_coa)):
-                    sum_coa += avg_coa[i]
-                av_coa = sum_coa / len(avg_coa)
-                print(av_coa)
-                final.append(int((av_coa / 5) * 100))
-                subject.append('COA')
-
-            # CALCULATIONS FOR cg
-            if Feedback.objects.all().filter(subject='Computer Graphics', user=request.user).count() != 0:
-                cg = Feedback.objects.all().filter(subject='Computer Graphics', user=request.user)
-                avg_cg = []
-                for i in range(0, len(cg)):
-                    sum = 0
-                    sum = cg[i].res1 + cg[i].res2 + cg[i].res3 + cg[i].res4 + cg[i].res5 + cg[i].res6 + cg[i].res7 + cg[i].res8 + cg[i].res9
-                    avg = sum / 9
-                    avg_cg.append(avg)
-                sum_cg = 0
-                for i in range(0, len(avg_cg)):
-                    sum_cg += avg_cg[i]
-                av_cg = sum_cg / len(avg_cg)
-                print(av_cg)
-                final.append(int((av_cg / 5) * 100))
-                subject.append('CG')
-
-            # CALCULATIONS FOR os
-            if Feedback.objects.all().filter(subject='Operating System', user=request.user).count() != 0:
-                os = Feedback.objects.all().filter(subject='Operating System', user=request.user)
-                avg_os = []
-                for i in range(0, len(os)):
-                    sum = 0
-                    sum = os[i].res1 + os[i].res2 + os[i].res3 + os[i].res4 + os[i].res5 + os[i].res6 + os[i].res7 + os[i].res8 + os[i].res9
-                    avg = sum / 9
-                    avg_os.append(avg)
-                sum_os = 0
-                for i in range(0, len(avg_os)):
-                    sum_os += avg_os[i]
-                av_os = sum_os / len(avg_os)
-                print(av_os)
-                final.append(int((av_os / 5) * 100))
-                subject.append('OS')
-
-            # CALCULATIONS FOR osl
-            if Feedback.objects.all().filter(subject='Open Source Tech Lab', user=request.user).count() != 0:
-                osl = Feedback.objects.all().filter(subject='Open Source Tech Lab', user=request.user)
-                avg_osl = []
-                for i in range(0, len(osl)):
-                    sum = 0
-                    sum = osl[i].res1 + osl[i].res2 + osl[i].res3 + osl[i].res4 + osl[i].res5 + osl[i].res6 + osl[i].res7 + osl[i].res8 + osl[i].res9
-                    avg = sum / 9
-                    avg_osl.append(avg)
-                sum_osl = 0
-                for i in range(0, len(avg_osl)):
-                    sum_osl += avg_osl[i]
-                av_osl = sum_osl / len(avg_osl)
-                print(av_osl)
-                final.append(int((av_osl / 5) * 100))
-                subject.append('OSL')
-
-            # FINAL PYTHON LIST
-            print(final)
-            print(subject)
+            for subject in subjects:
+                if Feedback.objects.all().filter(subject=subject).count() != 0:
+                    sub = Feedback.objects.all().filter(subject=subject, user=request.user)
+                    avg_sub = []
+                    for i in range(0, len(am3)):
+                        sum = 0
+                        sum = sub[i].res1 + sub[i].res2 + sub[i].res3 + sub[i].res4 + sub[i].res5 + sub[i].res6 + sub[i].res7 + sub[i].res8 + sub[i].res9
+                        avg = sum / 9
+                        avg_sub.append(avg)
+                    sum_sub = 0
+                    for i in range(0, len(avg_am3)):
+                        sum_am3 += avg_am3[i]
+                    av_am3 = sum_am3 / len(avg_am3)
+                    print(av_am3)
+                    final.append(int((av_am3 / 5) * 100))
+                    subject_list.append(subject[0])
             context = {
-                'feedback': final,
-                'subject': subject,
-                'f': ''
-            }
+                    'feedback': final,
+                    'subject': subject,
+                    'f': ''
+                }
             return render(request, 'appOne/analytics.html', context)
         else:
             return redirect('signup')
     else:
         if request.user.is_authenticated():
-            return render(request, 'appOne/analytics.html', {'f': 'No Feedbacks Yet!!'})
+            return render(request, 'appOne/analytics.html', {'f': 'No Fucks Yet!!'})
         else:
             return redirect('signup')
+
+
+        # if request.user.is_authenticated():
+        #     final = []
+        #     subject = []
+        #     # CALCULATIONS FOR AM3
+        #     if Feedback.objects.all().filter(subject='Applied Mathematics - III', user=request.user).count() != 0:
+        #         am3 = Feedback.objects.all().filter(subject='Applied Mathematics - III', user=request.user)
+        #         avg_am3 = []
+        #         for i in range(0, len(am3)):
+        #             sum = 0
+        #             sum = am3[i].res1 + am3[i].res2 + am3[i].res3 + am3[i].res4 + am3[i].res5 + am3[i].res6 + am3[i].res7 + am3[i].res8 + am3[i].res9
+        #             avg = sum / 9
+        #             avg_am3.append(avg)
+        #         sum_am3 = 0
+                # for i in range(0, len(avg_am3)):
+                #     sum_am3 += avg_am3[i]
+                # av_am3 = sum_am3 / len(avg_am3)
+                # print(av_am3)
+                # final.append(int((av_am3 / 5) * 100))
+                # subject.append('AM3')
+
+        #     # CALCULATIONS FOR AOA
+        #     if Feedback.objects.all().filter(subject='Analysis of Algorithms', user=request.user).count() != 0:
+        #         aoa = Feedback.objects.all().filter(subject='Analysis of Algorithms', user=request.user)
+        #         avg_aoa = []
+        #         for i in range(0, len(aoa)):
+        #             sum = 0
+        #             sum = aoa[i].res1 + aoa[i].res2 + aoa[i].res3 + aoa[i].res4 + aoa[i].res5 + aoa[i].res6 + aoa[i].res7 + aoa[i].res8 + aoa[i].res9
+        #             avg = sum / 9
+        #             avg_aoa.append(avg)
+        #         sum_aoa = 0
+        #         for i in range(0, len(avg_aoa)):
+        #             sum_aoa += avg_aoa[i]
+        #         av_aoa = sum_aoa / len(avg_aoa)
+        #         print(av_aoa)
+        #         final.append(int((av_aoa / 5) * 100))
+        #         subject.append('AOA')
+
+        #     # CALCULATIONS FOR coa
+        #     if Feedback.objects.all().filter(subject='Computer Organization and Architecture', user=request.user).count() != 0:
+        #         coa = Feedback.objects.all().filter(subject='Computer Organization and Architecture', user=request.user)
+        #         avg_coa = []
+        #         for i in range(0, len(coa)):
+        #             sum = 0
+        #             sum = coa[i].res1 + coa[i].res2 + coa[i].res3 + coa[i].res4 + coa[i].res5 + coa[i].res6 + coa[i].res7 + coa[i].res8 + coa[i].res9
+        #             avg = sum / 9
+        #             avg_coa.append(avg)
+        #         sum_coa = 0
+        #         for i in range(0, len(avg_coa)):
+        #             sum_coa += avg_coa[i]
+        #         av_coa = sum_coa / len(avg_coa)
+        #         print(av_coa)
+        #         final.append(int((av_coa / 5) * 100))
+        #         subject.append('COA')
+
+        #     # CALCULATIONS FOR cg
+        #     if Feedback.objects.all().filter(subject='Computer Graphics', user=request.user).count() != 0:
+        #         cg = Feedback.objects.all().filter(subject='Computer Graphics', user=request.user)
+        #         avg_cg = []
+        #         for i in range(0, len(cg)):
+        #             sum = 0
+        #             sum = cg[i].res1 + cg[i].res2 + cg[i].res3 + cg[i].res4 + cg[i].res5 + cg[i].res6 + cg[i].res7 + cg[i].res8 + cg[i].res9
+        #             avg = sum / 9
+        #             avg_cg.append(avg)
+        #         sum_cg = 0
+        #         for i in range(0, len(avg_cg)):
+        #             sum_cg += avg_cg[i]
+        #         av_cg = sum_cg / len(avg_cg)
+        #         print(av_cg)
+        #         final.append(int((av_cg / 5) * 100))
+        #         subject.append('CG')
+
+        #     # CALCULATIONS FOR os
+        #     if Feedback.objects.all().filter(subject='Operating System', user=request.user).count() != 0:
+        #         os = Feedback.objects.all().filter(subject='Operating System', user=request.user)
+        #         avg_os = []
+        #         for i in range(0, len(os)):
+        #             sum = 0
+        #             sum = os[i].res1 + os[i].res2 + os[i].res3 + os[i].res4 + os[i].res5 + os[i].res6 + os[i].res7 + os[i].res8 + os[i].res9
+        #             avg = sum / 9
+        #             avg_os.append(avg)
+        #         sum_os = 0
+        #         for i in range(0, len(avg_os)):
+        #             sum_os += avg_os[i]
+        #         av_os = sum_os / len(avg_os)
+        #         print(av_os)
+        #         final.append(int((av_os / 5) * 100))
+        #         subject.append('OS')
+
+        #     # CALCULATIONS FOR osl
+        #     if Feedback.objects.all().filter(subject='Open Source Tech Lab', user=request.user).count() != 0:
+        #         osl = Feedback.objects.all().filter(subject='Open Source Tech Lab', user=request.user)
+        #         avg_osl = []
+        #         for i in range(0, len(osl)):
+        #             sum = 0
+        #             sum = osl[i].res1 + osl[i].res2 + osl[i].res3 + osl[i].res4 + osl[i].res5 + osl[i].res6 + osl[i].res7 + osl[i].res8 + osl[i].res9
+        #             avg = sum / 9
+        #             avg_osl.append(avg)
+        #         sum_osl = 0
+        #         for i in range(0, len(avg_osl)):
+        #             sum_osl += avg_osl[i]
+        #         av_osl = sum_osl / len(avg_osl)
+        #         print(av_osl)
+        #         final.append(int((av_osl / 5) * 100))
+        #         subject.append('OSL')
+
+        #     # FINAL PYTHON LIST
+        #     print(final)
+        #     print(subject)
+            # context = {
+            #     'feedback': final,
+            #     'subject': subject,
+            #     'f': ''
+            # }
+        #     return render(request, 'appOne/analytics.html', context)
+        # else:
+        #     return redirect('signup')
+    
 
 
 def teacher_analytics(request):
